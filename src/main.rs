c1be1ae36e6eac07d4e4ae551e9f33162594eda7
Valentin Roland <valentin@vroland.de>
2018-05-07 17:10:32 +0200

    added command line parsing for parse_bookmap

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,48 @@
 extern crate mfnf_sitemap;
+extern crate serde_yaml;
+#[macro_use]
+extern crate structopt;
+
+use std::path::PathBuf;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::io;
+use structopt::StructOpt;
+
+/// Convert a sitemap MediaWiki article to a bookmap yaml.
+///
+/// Markers are propagated according to the marker inheritance rules.
+#[derive(StructOpt, Debug)]
+#[structopt(name = "mediawiki_bookmap")]
+struct Opt {
+    /// Input MediaWiki file
+    #[structopt(short = "i", long = "input", parse(from_os_str))]
+    input_file: Option<PathBuf>
+}
 
 fn main() {
-    println!("{:#?}", mfnf_sitemap::parse_sitemap(r#"
-= Beispielbuch =
-* include:
-** all
-** print
-* exclude:
-** minimal:
-*** Hey bla!
-*** Hey ho!
-* todo: Hello World!
+    let opt = Opt::from_args();
 
-== Grundlegende Formatierungen von Wikibooks ==
-* [[Mathe für Nicht-Freaks: Beispielkapitel: Grundlegende Formatierungen|Grundlegende Formatierungen]]
+    let mut input = String::new();
+    match opt.input_file {
+        Some(path) => {
+            BufReader::new(
+                File::open(&path).expect("Could not open input file!")
+            ).read_to_string(&mut input)
+                .expect("Could not read input file!");
+        }
+        None => {
+            BufReader::new(io::stdin())
+            .read_to_string(&mut input)
+                .expect("Could not read input file!");
+        }
+    };
 
-== Eigene Formatierungen ==
-* include:
-** minimal
-
-* [[Mathe für Nicht-Freaks: Beispielkapitel: Semantische Blöcke|Semantische Blöcke]]
-
-== Noprint-Content ==
-* [[Mathe für Nicht-Freaks: Beispielkapitel: Inhalte im Druck unterbinden|Inhalte im Druck unterbinden]]
-** include:
-*** minimal
-** exclude:
-*** print:
-**** Parameter von Vorlagen löschen
-"#));
+    println!("{}",
+        serde_yaml::to_string(
+            &mfnf_sitemap::parse_sitemap(&input)
+                .expect("Error parsing sitemap:")
+        ).expect("Could not serialize sitemap:")
+    );
 }
